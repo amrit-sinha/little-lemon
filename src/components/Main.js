ab998c3b5b60727bc2dbd6bc5ecdd6ddf1738cd9
amrit-sinha <amrit-sinha@outlook.com>
2024-03-22 02:42:58 +0530

    Specials component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,16 @@
 import RestaurantFood from "../assets/restauranfood.jpg";
-import GreekSalad from "../assets/greek salad.jpg";
-import Bruchetta from "../assets/bruchetta.svg";
-import LemonDessert from "../assets/lemon dessert.jpg";
 import Button from "./Button";
+import Description from "./Description.json";
 const Main = () => {
   const styles = {
     logo: {
       height: "200px",
       width: "200px",
+    },
+    spcl: {
+      display: "flex",
+      justifyContent: "space-between",
+      gap: "10px",
     },
   };
   return (
@@ -15,18 +18,9 @@ const Main = () => {
       <article>
         <p>Little Lemon</p>
         <p>Chicago</p>
-        <p>
-          We are a family owned Mediterranean restaurant, focused on traditional
-          recipes served with a modern twist
-        </p>
+        <p>{Description.TitleDescription}</p>
         <Button text="Reserve a Table" />
         <img style={styles.logo} src={RestaurantFood} alt="Restaurant food" />
-      </article>
-      <article>
-      <Button text="Online Menu" />
-        <img style={styles.logo} src={GreekSalad} alt="GreekSalad" />
-        <img style={styles.logo} src={Bruchetta} alt="GreekSalad" />
-        <img style={styles.logo} src={LemonDessert} alt="GreekSalad" />
       </article>
     </main>
   );
